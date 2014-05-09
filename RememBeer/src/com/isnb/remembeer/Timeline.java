67e7645bc9304754e9d9acfa9b50973cdeefb377
tgdlosa4 <tgdlosa4@U236975.corproot.net>
2014-05-09 10:59:48 +0200

    Fehler behoben

--- RememBeer/src/com/isnb/remembeer/Timeline.java
+++ RememBeer/src/com/isnb/remembeer/Timeline.java
@@ -1,5 +1,8 @@
 package com.isnb.remembeer;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 
 public class Timeline extends Activity {
 
@@ -9,10 +12,9 @@ public class Timeline extends Activity {
 		setContentView(R.layout.activity_timeline);
 	}
 
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.timeline, menu);
+		//getMenuInflater().inflate(R.menu.timeline, menu);
 		return true;
 	}
 
