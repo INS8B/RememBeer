82673eb7befabeabe7194d826616cfcbb6a945b0
Tamino <tamhelbling@gmail.com>
2014-05-15 13:35:00 +0200

    Screen Rotation gestoppt Hintergrund ausprobiert

--- RememBeer/src/com/isnb/remembeer/Menu.java
+++ RememBeer/src/com/isnb/remembeer/Menu.java
@@ -2,6 +2,7 @@ package com.isnb.remembeer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -13,7 +14,8 @@ public class Menu extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_menu);
 	}
 	
 	/** Called when the user touches the button */
